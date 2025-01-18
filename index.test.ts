452bd9ebd80fee4286400385ce92d2034c276d07
silverwind <me@silverwind.io>
2025-01-18 17:59:04 +0100

    add test

--- index.test.ts
+++ index.test.ts
@@ -1,10 +1,13 @@
 import {yamlPlugin} from "./index.ts";
 import petstore from "./fixtures/petstore.yaml";
+import {load} from "js-yaml";
+import {readFile} from "node:fs/promises";
 
 test("exists", () => {
   expect(yamlPlugin).toBeFunction();
 });
 
-test("yaml", () => {
+test("petstore", async () => {
   expect(petstore).toMatchSnapshot();
+  expect(load(await readFile("fixtures/petstore.yaml", "utf8"))).toEqual(petstore);
 });
